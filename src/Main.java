04541b8ba88c37d2fbda0bd3f3c15ae45873b297
enik <ennik695@yandex.ru>
2025-02-13 11:50:12 +0300

    написал новый тест, в мэйне создал объекты задачь и обновленных задачь , их и передаю менеджеру

--- src/Main.java
+++ src/Main.java
@@ -4,62 +4,82 @@ public class Main {
         TaskManager taskManager = new TaskManager();
 
         //создаем две задачи
-        taskManager.newTask(new Task("Купить лего набор", "Малому нужен набор лего звездные войны" +
-                " Явин-4", Status.NEW));
-        taskManager.newTask(new Task("Закончить спринт", "надо бы поторопиться", Status.NEW));
+        Task task1 = new Task("Купить лего набор", "Малому нужен набор лего звездные войны" +
+                " Явин-4", Status.NEW);
+        Task task2 = new Task("Закончить спринт", "надо бы поторопиться", Status.NEW);
+
+        taskManager.newTask(task1);
+        taskManager.newTask(task2);
 
         //создаем два эпика
-        taskManager.newEpic(new Epic("Сделать ремонт","страшно, очень страшно, если бы мы знали что" +
-                " это такое..."));
-        taskManager.newEpic(new Epic("Поменять лампочку в авто","Надо бы это сделать"));
+        Epic epic1 = new Epic("Сделать ремонт","страшно, очень страшно, если бы мы знали что" +
+                " это такое...");
+        Epic epic2 = new Epic("Поменять лампочку в машине","Нужно всетаки это сделать");
+        taskManager.newEpic(epic1);
+        taskManager.newEpic(epic2);
 
         //добавляем подзадачи
-        taskManager.newSubTask(new SubTask("C счего начать?", "въехать в стройку", Status.NEW,
-                2));
-        taskManager.newSubTask(new SubTask("Дипломатия", "Убедить жену что все и так ок", Status.NEW,
-                2));
-        taskManager.newSubTask(new SubTask("Купить лампочку", "дойти до магазина и купить", Status.NEW,
-                3));
+        SubTask subTask1 = new SubTask("C счего начать?", "въехать в стройку", Status.NEW,
+                2);
+        SubTask subTask2 = new SubTask("Дипломатия", "Убедить жену что все и так ок", Status.NEW,
+                2);
+        SubTask subTask3 = new SubTask("Купить лампочку", "дойти до магазина и купить", Status.NEW,
+                3);
+        taskManager.newSubTask(subTask1);
+        taskManager.newSubTask(subTask2);
+        taskManager.newSubTask(subTask3);
 
-        System.out.println(taskManager.listTask());
-        System.out.println(taskManager.listEpic());
-        System.out.println(taskManager.listSubTask());
-        //System.out.println();
-       // System.out.println();
+        System.out.println(taskManager.getTaskList());
+        System.out.println(taskManager.getEpicList());
+        System.out.println(taskManager.getSubTaskList());
+        System.out.println();
+        System.out.println();
 
+// для обновления задачь создадим новые объекты -- ~копии уже существующих c изменениями
+        Task newTask1 = new Task("Купить лего набор", "Малому нужен набор лего звездные войны" +
+                " Явин-4.Почти купил", Status.IN_PROGRESS);
+        newTask1.setId(task1.getId());
+        Task newTask2 = new Task("Закончить спринт", "ускорился", Status.IN_PROGRESS);
+        newTask2.setId(task2.getId());
+
+        Epic newEpic2 = new Epic("Поменять лампочку в авто","купил, но возможн не ту");
+        newEpic2.setId(epic2.getId());
+
+        SubTask newSubTask1 = new SubTask("C счего начать?", "въехать в стройку",
+                Status.DONE, 2);
+        newSubTask1.setId(subTask1.getId());
+        SubTask newSubTask2 = new SubTask("Дипломатия", "Убедить жену что все и так" +
+                " ок", Status.DONE, 2);
+        newSubTask2.setId(subTask2.getId());
+        SubTask newSubTask3 = new SubTask("Купить лампочку", "дойти до магазина" +
+                " и купить", Status.IN_PROGRESS, 3);
+        newSubTask3.setId(subTask3.getId());
 
         //меняем  задачи
-        taskManager.updateTask(0, new Task("Купить лего набор", "Малому нужен набор лего" +
-                " звездные войны Явин-4! Почти купил!!!", Status.IN_PROGRESS));
-        taskManager.updateTask(1, new Task("Закончить спринт", "ускорился", Status.IN_PROGRESS));
+        taskManager.updateTask(newTask1);
+        taskManager.updateTask(newTask2);
 
         //меняем подзадачи
-        taskManager.updateSubTask(4, new SubTask("C счего начать?", "въехать в стройку",
-                Status.DONE, 2));
-        taskManager.updateSubTask(5, new SubTask("Дипломатия", "Убедить жену что все и так" +
-                " ок", Status.DONE, 2));
-        taskManager.updateSubTask(6, new SubTask("Купить лампочку", "дойти до магазина" +
-                " и купить", Status.IN_PROGRESS, 3));
+        taskManager.updateSubTask(newSubTask1);
+        taskManager.updateSubTask(newSubTask2);
+        taskManager.updateSubTask(newSubTask3);
 
         //меняем описание в эпике
-        taskManager.updateEpic(3, new Epic("Поменять лампочку в авто","купил, но возможн не ту"));
+        taskManager.updateEpic(newEpic2);
 
-        //System.out.println(taskManager.listTask());
-       // System.out.println(taskManager.getEpicById(2));
-        //System.out.println(taskManager.getSubTasksForEpic(2));
-        //System.out.println(taskManager.getEpicById(3));
-        //System.out.println(taskManager.getSubTasksForEpic(3));
-        //System.out.println();
-        //System.out.println();
+        System.out.println(taskManager.getTaskList());
+        System.out.println(taskManager.getEpicById(2));
+        System.out.println(taskManager.getSubTasksForEpic(2));
+        System.out.println(taskManager.getEpicById(3));
+        System.out.println(taskManager.getSubTasksForEpic(3));
+        System.out.println();
+        System.out.println();
 
         //удаление
-        //taskManager.deleteEpic(2);
-       // taskManager.deleteTask(1);
-        //System.out.println(taskManager.listTask());
-        //System.out.println(taskManager.listEpic());
-        //System.out.println(taskManager.listSubTask());
-
-
-
+        taskManager.deleteEpic(2);
+        taskManager.deleteTask(1);
+        System.out.println(taskManager.getTaskList());
+        System.out.println(taskManager.getEpicList());
+        System.out.println(taskManager.getSubTaskList());
     }
 }
